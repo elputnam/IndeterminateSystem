61ba95bfa14d16b6125b2b7b096df495b5f61d9d
elputnam <elputnam@inaction.ie>
2021-11-14 18:22:56 +0000

    updated and added more images

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ var photos = [];
 var img; 
 
 function preload(){
-  for (let i = 1; i < 14; i++){
-    photos[i] = loadImage("data/maternalDance-" + i + ".jpg");
+  for (let i = 1; i < 91; i++){
+    photos[i] = loadImage("data/maternalDance-" + i + ".png");
   }  
 }
 
@@ -15,7 +15,7 @@ function setup() {
 
 function draw() {
   // background(175, random(100), random(100), 10);
-  let num = int(random(1, 14));
+  let num = int(random(1, 90));
   tint(random(165, 190), 100, 100, random(100));
   image(photos[num], 0, 0, width, height);
 }
