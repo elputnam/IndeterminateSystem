426a25fbcdd27cc1620919cbe728a5f14fd1a240
elputnam <elputnam@inaction.ie>
2021-11-14 18:01:13 +0000

    initial commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var photos = [];
+var img; 
+
+function preload(){
+  for (let i = 1; i < 14; i++){
+    photos[i] = loadImage("data/maternalDance-" + i + ".jpg");
+  }  
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(20);
+  colorMode(HSB, 360, 100, 100, 100);
+}
+
+function draw() {
+  // background(175, random(100), random(100), 10);
+  let num = int(random(1, 14));
+  tint(random(165, 190), 100, 100, random(100));
+  image(photos[num], 0, 0, width, height);
+}
